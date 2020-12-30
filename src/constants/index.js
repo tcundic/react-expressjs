dd1fc9dca533880a2d883b3df40b9c5a74046612
Tomislav Cundic <tcundic@gmail.com>
2020-12-30 12:07:21 +0100

    Port changes (production)

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 const port = process.env.PORT || 3000;
-const API_BASE_URL = `http://localhost/API`;
+const API_BASE_URL = `https://reactjs-expressjs.herokuapp.com/API`;
 
 export default API_BASE_URL;

3c3fc09efd523ef6a61b535f0465ff1b4b06d6cd
Tomislav Cundic <tcundic@gmail.com>
2020-12-30 12:10:37 +0100

    Changed port for local app

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 const port = process.env.PORT || 3000;
-const API_BASE_URL = `https://reactjs-expressjs.herokuapp.com/API`;
+const API_BASE_URL = `http://localhost:${port}/API`;
 
 export default API_BASE_URL;
